4c946d1811aee4f5392cc9f9fb60b85500023c7c
yefimovych <yuliia.yefimovych@gmail.com>
2023-04-14 14:31:18 +0300

    mathematical operations

---
+++ index.js
@@ -0,0 +1,12 @@
+let num1 = prompt ("Введіть перше число:");
+let num2 = prompt("Введіть друге число:");
+
+let sum = Number(num1) + Number(num2);
+let minus = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+alert(`${num1}+${num2}=${sum}\n
+${num1}-${num2}=${minus}\n
+${num1}*${num2}=${product}\n
+${num1}/${num2}=${quotient}`);
