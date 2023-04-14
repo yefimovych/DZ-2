250288ccdf950241483b4450010fc1fbaeb2f0d9
yefimovych <yuliia.yefimovych@gmail.com>
2023-04-14 14:38:14 +0300

    mathematical operations

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let num1 = prompt ("Введіть перше число:");
 let num2 = prompt("Введіть друге число:");
 
 let sum = Number(num1) + Number(num2);
+console.log(typeof num1 )
 let minus = num1 - num2;
 let product = num1 * num2;
 let quotient = num1 / num2;
