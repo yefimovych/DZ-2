ba0356cdb1d6adf9f4b77926c4373cae0fac9de6
yefimovych <yuliia.yefimovych@gmail.com>
2023-05-16 15:48:35 +0300

    DZ.10/1 Пошук у масиві

--- index.js
+++ index.js
@@ -102,34 +102,87 @@ alert(` ${hours} годин = ${seconds} секунд`);
 
 // console.log("Вміст масиву:", arr);
 
-let newNumber = ''; 
-for (let i = 10; i <= 20; i++) { newNumber += i + ', '; } 
-console.log(newNumber);
-for (let i = 10; i <= 20; i++) {
-  console.log(i * i);
-}
-for (let i = 1; i <= 10; i++) {
-  console.log(7 * i);
-}
-let sum = 0;
-for (let i = 1; i <= 15; i++) {
-  sum += i;
-}
-console.log(sum);
-let product = 1;
-for (let i = 15; i <= 35; i++) {
-  product *= i;
-}
-console.log(product);
+// let newNumber = ''; 
+// for (let i = 10; i <= 20; i++) { newNumber += i + ', '; } 
+// console.log(newNumber);
+// for (let i = 10; i <= 20; i++) {
+//   console.log(i * i);
+// }
+// for (let i = 1; i <= 10; i++) {
+//   console.log(7 * i);
+// }
+// let sum = 0;
+// for (let i = 1; i <= 15; i++) {
+//   sum += i;
+// }
+// console.log(sum);
+// let product = 1;
+// for (let i = 15; i <= 35; i++) {
+//   product *= i;
+// }
+// console.log(product);
 
-let sum2 = 0;
-for (let i = 1; i <= 500; i++) {
-  sum2+= i;
-}
-let average = sum2 / 500;
-console.log(average);
-for (let i = 100; i <= 200; i++) {
-  if (i % 3 === 0) {
-    console.log(i);
+// let sum2 = 0;
+// for (let i = 1; i <= 500; i++) {
+//   sum2+= i;
+// }
+// let average = sum2 / 500;
+// console.log(average);
+// for (let i = 100; i <= 200; i++) {
+//   if (i % 3 === 0) {
+//     console.log(i);
+//   }
+// }
+
+const array = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
+
+let sum = 0;
+let positiveCount = 0;
+for (let i = 0; i < array.length; i++) {
+  if (array[i] > 0) {
+    sum += array[i];
+    positiveCount++;
   }
 }
+console.log('Сума позитивних елементів:', sum);
+console.log('Кількість позитивних елементів:', positiveCount);
+
+
+let minElement = array[0];
+let minIndex = 0;
+for (let i = 1; i < array.length; i++) {
+  if (array[i] < minElement) {
+    minElement = array[i];
+    minIndex = i;
+  }
+}
+console.log('Мінімальний елемент:', minElement);
+console.log('Порядковий номер мінімального елемента:', minIndex);
+
+
+let maxElement = array[0];
+let maxIndex = 0;
+for (let i = 1; i < array.length; i++) {
+  if (array[i] > maxElement) {
+    maxElement = array[i];
+    maxIndex = i;
+  }
+}
+console.log('Максимальний елемент:', maxElement);
+console.log('Порядковий номер максимального елемента:', maxIndex);
+
+let negativeCount = 0;
+for (let i = 0; i < array.length; i++) {
+  if (array[i] < 0) {
+    negativeCount++;
+  }
+}
+console.log('Кількість негативних елементів:', negativeCount);
+
+let product = 1;
+for (let i = 0; i < array.length; i++) {
+  if (array[i] > 0) {
+    product *= array[i];
+  }
+}
+console.log('Добуток позитивних елементів:', product);
