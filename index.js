a78442b620f640e4b198110fefe704fdf0b5c2ee
yefimovych <yuliia.yefimovych@gmail.com>
2023-05-22 14:47:18 +0300

    ДЗ 18. Багатоквартирний будинок

--- index.js
+++ index.js
@@ -134,55 +134,105 @@ alert(` ${hours} годин = ${seconds} секунд`);
 //   }
 // }
 
-const array = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
+// const array = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
 
-let sum = 0;
-let positiveCount = 0;
-for (let i = 0; i < array.length; i++) {
-  if (array[i] > 0) {
-    sum += array[i];
-    positiveCount++;
+// let sum = 0;
+// let positiveCount = 0;
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i] > 0) {
+//     sum += array[i];
+//     positiveCount++;
+//   }
+// }
+// console.log('Сума позитивних елементів:', sum);
+// console.log('Кількість позитивних елементів:', positiveCount);
+
+
+// let minElement = array[0];
+// let minIndex = 0;
+// for (let i = 1; i < array.length; i++) {
+//   if (array[i] < minElement) {
+//     minElement = array[i];
+//     minIndex = i;
+//   }
+// }
+// console.log('Мінімальний елемент:', minElement);
+// console.log('Порядковий номер мінімального елемента:', minIndex);
+
+
+// let maxElement = array[0];
+// let maxIndex = 0;
+// for (let i = 1; i < array.length; i++) {
+//   if (array[i] > maxElement) {
+//     maxElement = array[i];
+//     maxIndex = i;
+//   }
+// }
+// console.log('Максимальний елемент:', maxElement);
+// console.log('Порядковий номер максимального елемента:', maxIndex);
+
+// let negativeCount = 0;
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i] < 0) {
+//     negativeCount++;
+//   }
+// }
+// console.log('Кількість негативних елементів:', negativeCount);
+
+// let product = 1;
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i] > 0) {
+//     product *= array[i];
+//   }
+// }
+// console.log('Добуток позитивних елементів:', product);
+
+class Person {
+  constructor(name, gender) {
+    this.name = name;
+    this.gender = gender;
   }
 }
-console.log('Сума позитивних елементів:', sum);
-console.log('Кількість позитивних елементів:', positiveCount);
 
+class Apartment {
+  constructor() {
+    this.residents = [];
+  }
 
-let minElement = array[0];
-let minIndex = 0;
-for (let i = 1; i < array.length; i++) {
-  if (array[i] < minElement) {
-    minElement = array[i];
-    minIndex = i;
+  addResident(person) {
+    this.residents.push(person);
   }
 }
-console.log('Мінімальний елемент:', minElement);
-console.log('Порядковий номер мінімального елемента:', minIndex);
 
+class House {
+  constructor(maxApartments) {
+    this.apartments = [];
+    this.maxApartments = maxApartments;
+  }
 
-let maxElement = array[0];
-let maxIndex = 0;
-for (let i = 1; i < array.length; i++) {
-  if (array[i] > maxElement) {
-    maxElement = array[i];
-    maxIndex = i;
+  addApartment(apartment) {
+    if (this.apartments.length < this.maxApartments) {
+      this.apartments.push(apartment);
+    } else {
+      console.log("Досягнуто максимальну кількість квартир у будинку.");
+    }
   }
 }
-console.log('Максимальний елемент:', maxElement);
-console.log('Порядковий номер максимального елемента:', maxIndex);
 
-let negativeCount = 0;
-for (let i = 0; i < array.length; i++) {
-  if (array[i] < 0) {
-    negativeCount++;
-  }
-}
-console.log('Кількість негативних елементів:', negativeCount);
+const person1 = new Person("John", "male");
+const person2 = new Person("Emily", "female");
+const person3 = new Person("Michael", "male");
+const person4 = new Person("Sophia", "female");
 
-let product = 1;
-for (let i = 0; i < array.length; i++) {
-  if (array[i] > 0) {
-    product *= array[i];
-  }
-}
-console.log('Добуток позитивних елементів:', product);
+const apartment1 = new Apartment();
+const apartment2 = new Apartment();
+
+apartment1.addResident(person1);
+apartment1.addResident(person2);
+apartment2.addResident(person3);
+apartment2.addResident(person4);
+
+const house = new House(2);
+
+house.addApartment(apartment1);
+house.addApartment(apartment2);
